ecb3b8c6ad1e5e4ba45b9e07834f050c7a95a68a
shakhawat1355 <hossain.shakhawat@brainstation-23.com>
2023-04-13 15:32:49 +0600

    Add project files.

---
+++ testGit2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace testGit2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
